e5e3671d5b67f8ef09c28dd74f4951a083096ca9
Agustin Maciá <amaciadelrio@gmail.com>
2020-10-05 14:18:18 -0300

    Validaciones del login con Stream

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,1 +1,29 @@
 //Archivo para hacer validaciones en el login form
+
+import 'dart:async';
+
+class LoginBloc {
+
+  //Le especifico la informacion que va a pasar por el stream Controller. Con el broadcast puedo escuchar mas de una instancia
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+  //Recuperar datos del stream(Salida)
+  Stream<String> get emailStream => _emailController.stream;
+  Stream<String> get passwordStream => _passwordController.stream;
+  
+  //Agrego info al stream
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+
+  //Cerrar los streams. Uso dipose.
+  dispose() {
+    
+    //Con el ? valido que el valor no sea nulo
+    _emailController?.close();
+    _passwordController?.close();
+  }
+
+
+}
